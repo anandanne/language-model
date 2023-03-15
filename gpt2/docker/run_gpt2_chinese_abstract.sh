f20a4e2eeb2194909717f2b800390f931d06fab4
xusenlin <xusenlin@dnect.cn>
2023-03-15 17:27:20 +0800

    修改docker中训练数据格式

--- gpt2/docker/run_gpt2_chinese_abstract.sh
+++ gpt2/docker/run_gpt2_chinese_abstract.sh
@@ -2,8 +2,8 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python -m torch.distributed.launch --nproc_per_node 8 run_gpt2_chinese_abstract.py \
     --config_name model/config.json \
     --tokenizer_name model/vocab.txt \
-    --train_file data/train.txt \
-    --validation_file data/valid.txt \
+    --train_file data/train.json \
+    --validation_file data/valid.json \
     --cache_dir data \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
