7f8c9c3ea7003b70f3e1470b7230ab962d714916
xusenlin <xusenlin@dnect.cn>
2023-03-23 16:03:01 +0800

    add docker run

---
+++ docker/run.sh
@@ -0,0 +1,9 @@
+docker run -it -d --gpus all --ipc=host -p 80:80 --name=llm \
+    --ulimit memlock=-1 --ulimit stack=67108864 \
+    -e MODEL:checkpoints/gpt2-abstract/checkpoint-500000 \
+    -e SERVER_MODEL_NAME:gpt2-abstract \
+    -e MODEL_LOCAL_FILES_ONLY:true \
+    -e TOKENIZER_NAME:bert \
+    -e REMOVE_WHITESPACE:true \
+    -v `pwd`:/workspace \
+    pytorch-llm:gpu
