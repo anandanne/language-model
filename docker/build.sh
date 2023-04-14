1ea94de4164c7c0135e4106c6fe9192fc2200260
xusenlin <xusenlin@dnect.cn>
2023-04-14 22:58:41 +0800

    update docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
-docker build -t llm:gpu -f docker/Dockerfile .
+docker build -t llm:pytorch -f docker/Dockerfile .
--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 docker run -it -d --gpus all --ipc=host --net=host -p 80:80 -p:7860:7860 --name=llm \
     --ulimit memlock=-1 --ulimit stack=67108864 \
     -v `pwd`:/workspace \
-    llm:gpu
+    llm:pytorch
